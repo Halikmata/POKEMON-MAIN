395f839587c6f705fc20fa5351d8c7427064d402
Jason Maverick Diokno Serencio <202180383@psu.palawan.edu.ph>
2023-12-07 12:12:14 +0800

    initial commit

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -1,7 +1,7 @@
 """
 URL configuration for projectsite project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
+The urlpatterns list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
 Examples:
 Function views
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from cardquest.views import HomePageView, TrainerList
+from cardquest import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # path('', views.HomePageView.as_view(), name='home'),
+    # path('trainer_list', TrainerList.as_view(), name='trainer-list'),
 ]
