7b7cac790b428763cf51ac22f15f0ce12f93f32d
Jason Maverick Diokno Serencio <202180383@psu.palawan.edu.ph>
2023-12-07 22:08:55 +0800

    edited db

--- projectsite/cardquest/views.py
+++ projectsite/cardquest/views.py
@@ -1,3 +1,57 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic.list import ListView
+from cardquest.models import PokemonCard, Trainer, Collection
+
+
+class HomePageView(ListView):
+    model = PokemonCard
+    context_object_name = 'home'
+    template_name = "home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+
+class TrainerList(ListView):
+    model = Trainer
+    context_object_name = 'trainer'
+    template_name = 'trainer.html'
+    paginate_by = 5
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+    def get_queryset(self, *args, **kwargs):
+        qs = super(TrainerList, self).get_queryset(*args, **kwargs)
+        return qs
+    
+class PokemonCard(ListView):
+    model = PokemonCard
+    context_object_name = 'pokemon card'
+    template_name = 'pokemon-card.html'
+    paginate_by = 5
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+    def get_queryset(self, *args, **kwargs):
+        qs = super(PokemonCard, self).get_queryset(*args, **kwargs)
+        return qs
+    
+class Collection(ListView):
+    model = Collection
+    context_object_name = 'collection'
+    template_name = 'collection.html'
+    paginate_by = 5
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+    def get_queryset(self, *args, **kwargs):
+        qs = super(Collection, self).get_queryset(*args, **kwargs)
+        return qs
--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from cardquest.views import HomePageView, TrainerList
+from cardquest import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.HomePageView.as_view(), name='home'),
+    path('trainer_list', TrainerList.as_view(), name='trainer-list'),
 ]
