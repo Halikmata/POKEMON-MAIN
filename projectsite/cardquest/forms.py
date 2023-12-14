167fa3d13262a7d865bca4edb036354b886cc432
Jason Maverick Diokno Serencio <202180383@psu.palawan.edu.ph>
2023-12-15 00:58:57 +0800

    added TrainerForm

--- projectsite/cardquest/forms.py
+++ projectsite/cardquest/forms.py
@@ -1,8 +1,8 @@
 from django.forms import ModelForm
 from django import forms
 from .models import Trainer, Collection, PokemonCard
-class TrainerForm (ModelForm):
-    birthdate = forms.DateField (widget=forms.DateInput (attrs = { 'type': 'date'})) 
+class TrainerForm(ModelForm):
+    birthdate = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'})) 
     class Meta:
         model = Trainer
-        fields = all
+        fields = "__all__"
